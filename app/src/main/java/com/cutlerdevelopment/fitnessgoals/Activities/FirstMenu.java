3b4c14babebabcd682f9147b0c8fbf43d3ae20d4
Brendan <brendan.cutler1992@gmail.com>
2020-04-20 12:32:25 +0100

    Quick tidy up and doc comments.

--- app/src/main/java/com/cutlerdevelopment/fitnessgoals/Activities/FirstMenu.java
+++ app/src/main/java/com/cutlerdevelopment/fitnessgoals/Activities/FirstMenu.java
@@ -50,6 +50,10 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * FirstMenu Activity is the first Activity that loads when the User first opens the app. It's a setup menu.
+ * The class is organised into the different steps, overridden activity results then some animation methods.
+ */
 public class FirstMenu extends AppCompatActivity implements IntegrationConnectionHandler.IntegrationListener {
 
     Boolean readyToProgress = false;
@@ -131,6 +135,10 @@ public class FirstMenu extends AppCompatActivity implements IntegrationConnectio
 
     }
 
+    /**
+     * nextStep is called whenever the User presses anywhere on the screen.
+     * If the readyToProgress boolean is true a switch statement checks which step the user is on, updates the UI, sets readyToProgress to false and increases tutorialStep.
+     */
     void nextStep() {
 
         if (readyToProgress) {
@@ -211,27 +219,191 @@ public class FirstMenu extends AppCompatActivity implements IntegrationConnectio
         }
     }
 
+    /**
+     * Populates the fitness app list with all available apps as a fitness_app_small_card.
+     * FitnessApps.NUM_OF_APPS controls how many items it populates with.
+     * Also assigns an onItemClickListener that calls selectFitnessApp
+     */
+    void populateFitnessAppList() {
+        final ArrayList<FitnessAppSmallCard> fitnessApps = new ArrayList<>();
+        boolean offsetColour = false;
 
+        for (int i = 0; i < FitnessApps.NUM_OF_APPS; i++) {
+            FitnessAppSmallCard item = new FitnessAppSmallCard();
+            item.setAppName(FitnessApps.getFitnessAppName(i));
+            item.setAppImage(FitnessApps.getFitnessAppImage(i));
+            if (offsetColour) {
+                item.setBackgroundColour(R.color.colorPrimary);
+                offsetColour = false;
+            }
+            else {
+                item.setBackgroundColour(R.color.colorPrimary);
+                offsetColour = true;
+            }
+
+            fitnessApps.add(item);
+
+        }
+
+        final FitnessAppSmallCardAdapter adapter = new FitnessAppSmallCardAdapter(getApplicationContext(), fitnessApps);
+        fitnessAppsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                FitnessAppSmallCard item = (FitnessAppSmallCard) adapterView.getItemAtPosition(i);
+                selectFitnessApp(FitnessApps.getFitnessAppFromName(item.getAppName()));
+            }
+        });
+        fitnessAppsList.setAdapter(adapter);
+
+        ViewGroup.LayoutParams layoutParams = fitnessAppsLayout.getLayoutParams();
+        layoutParams.width = (int) (jamesImage.getX() - Numbers.FIRST_MENU_FITNESS_MARGIN_PADDING);
+        fitnessAppsLayout.setLayoutParams(layoutParams);
+        fitnessAppsLayout.requestLayout();
+
+    }
+
+    /**
+     * Populates the teamNameColour spinner.
+     * If they've chosen Team Mode the spinner is populated with colours, controlled by Colours.NUM_TEAM_COLOURS.
+     * If they've chosen Player Mode the spinner is populated wit team names. List of names taken from Words.teamNames.
+     */
+    void populateSpinner() {
+        List<String> spinnerList = new ArrayList<>();
+
+        if (gameMode == GameModes.TEAM_MODE) {
+            for (int i = 1; i <= Colours.NUM_TEAM_COLOURS; i++) {
+                spinnerList.add(Colours.getColourName(i));
+            }
+        }
+        else {
+            spinnerList = Words.teamNames;
+            Collections.sort(spinnerList);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, R.layout.spinner_item, spinnerList);
+        teamColourSpinner.setAdapter(adapter);
+    }
+
+
+    /**
+     * Populates the stepTarget spinner.
+     * Starts at 1 and loops through to Numbers.MAX_NUM_STEPS_TARGET
+     */
+    void populateStepTargetSpinner() {
+        List<String> spinnerList = new ArrayList<>();
+        for (int i = 1; i <= Numbers.MAX_NUM_STEPS_TARGET; i++) {
+            spinnerList.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> stepAdapter = new ArrayAdapter<>(
+                this, R.layout.spinner_item, spinnerList);
+        stepTargetSpinner.setAdapter(stepAdapter);
+    }
+
+
+    /**
+     * Populates the daysBetween spinner.
+     * Starts at 2 and increments by 2, up to and including Numbers.MAX_NUM_DAYS_BETWEEN
+     */
+    void populateDaysBetweenSpinner() {
+        List<String> spinnerList = new ArrayList<>();
+        for (int i = 2; i <= Numbers.MAX_NUM_DAYS_BETWEEN; i+=2) {
+            spinnerList.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> daysAdapter = new ArrayAdapter<>(
+                this, R.layout.spinner_item, spinnerList);
+        daysBetweenSpinner.setAdapter(daysAdapter);
+    }
+
+
+
+    /**
+     * WHen a fitness app is selected, selectFitnessApp is called.
+     * If Mocked is selected, it immediately confirms app connected.
+     * If GoogleFit is selected, will create an instance of the GoogleFitAPI and check permissions.
+     * If we have permission, will confirm app connected. If not, GoogleFitAPI will request permissions.
+     * If Fitbit is selected, will create an instance of the FitbitStringsSavedData and FitbitAPI.
+     * Will then request permission from FitbitAPI.
+     * @param app the int value of the app selected, will be compared to FitnessApps.
+     */
+    void selectFitnessApp(int app) {
+        switch (app) {
+            case FitnessApps.MOCKED:
+                hideMenu(fitnessAppsLayout);
+                putSpeechBubbleBackIntoMiddle();
+                confirmAppConnected();
+                break;
+            case FitnessApps.GOOGLE_FIT:
+                speechBubbleText.setText(R.string.first_menu_calling_app);
+                hideMenu(fitnessAppsLayout);
+                putSpeechBubbleBackIntoMiddle();
+                GoogleFitAPI.createGoogleFitAPIInstance(this);
+                if (GoogleFitAPI.getInstance().hasPermissions(this)) {
+                    confirmAppConnected();
+                }
+                break;
+            case FitnessApps.FITBIT:
+                speechBubbleText.setText(R.string.first_menu_calling_app);
+                hideMenu(fitnessAppsLayout);
+                putSpeechBubbleBackIntoMiddle();
+                FitbitStringsSavedData.createFitbitStringsSavedDataInstance(this);
+                FitbitAPI.createFitbitAPIInstance(this);
+                FitbitAPI.getInstance().requestFitbitPermission(this);
+                break;
+            default:
+                speechBubbleText.setText(R.string.first_menu_app_connected);
+        }
+        Settings.getInstance().changeFitnessApp(app);
+
+    }
+
+    /**
+     * Sets speech bubble text to first_menu_app_connected String
+     * Sets the app ready to progress
+     */
     void confirmAppConnected() {
         speechBubbleText.setText(getString(R.string.first_menu_app_connected));
         readyToProgress = true;
     }
+
+    /**
+     * Sets speech bubble text to first_menu_app_cant_connect String
+     * Shows fitnessAppsLayout
+     * Moves speech bubble to the right
+     */
     void confirmCantConnect() {
         speechBubbleText.setText(getString(R.string.first_menu_app_cant_connect));
         showMenu(fitnessAppsLayout);
         pushSpeechBubbleToRight(fitnessAppsLayout);
     }
 
+    /**
+     * Sets speech bubble text to team_mode_details String
+     * Changes background colour of teamMode to primaryDark
+     * Changes background colour of playerMode to black
+     */
     public void whatIsTeamMode(View view) {
         speechBubbleText.setText(getString(R.string.team_mode_details));
         teamModeBackgroundColour.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
         playerModeBackgroundColour.setBackgroundColor(getResources().getColor(R.color.colorBlack));
     }
+
+    /**
+     * Sets speech bubble text to player_mode_details String
+     * Changes background colour of teamMode to black
+     * Changes background colour of playerMode to primaryDark
+     */
     public void whatIsPlayerMode(View view) {
         speechBubbleText.setText(getString(R.string.player_mode_details));
         teamModeBackgroundColour.setBackgroundColor(getResources().getColor(R.color.colorBlack));
         playerModeBackgroundColour.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
     }
+
+    /**
+     * Sets speech bubble text to first_menu_game_mode_selected String and passes the words manager and team
+     * Hides playerModeLayout
+     * Moves speech bubble above jamesImage
+     */
     public void selectTeamMode(View view) {
         gameMode = GameModes.TEAM_MODE;
         hideMenu(playerModeLayout);
@@ -240,6 +412,12 @@ public class FirstMenu extends AppCompatActivity implements IntegrationConnectio
         readyToProgress = true;
 
     }
+
+    /**
+     * Sets speech bubble text to first_menu_game_mode_selected String and passes the words player and player
+     * Hides playerModeLayout
+     * Moves speech bubble above jamesImage
+     */
     public void selectPlayerMode(View view) {
         gameMode = GameModes.PLAYER_MODE;
         hideMenu(playerModeLayout);
@@ -351,6 +529,10 @@ public class FirstMenu extends AppCompatActivity implements IntegrationConnectio
 
     }
 
+    /**
+     * Method called by IntegrationConnectionHandler.ConnectionListener. Is called when we've had response from appropriate API class with average
+     * @param average the number returned by API. Can be 0 when there are problems.
+     */
     @Override
     public void getAverageSteps(int average) {
         if (average > 0) { speechBubbleText.setText(getString(R.string.first_menu_got_average, "7", Words.getNumberWithCommas(average))); }
@@ -358,6 +540,38 @@ public class FirstMenu extends AppCompatActivity implements IntegrationConnectio
         showMenu(stepTargetLayout);
         pushSpeechBubbleToRight(stepTargetLayout);
 
+    }
+
+    /**
+     * When GoogleFit or FitbitAPI get authorisation we startActivity for result with a defined Request Code from FitnessApps.
+     * Depending on result, will either confirmAppConnected or confirmCantConnect
+     * @param requestCode 
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == FitnessApps.GOOGLE_FIT_INIT_REQUEST_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            int statusCode = result.getStatus().getStatusCode();
+            if (statusCode == GoogleSignInStatusCodes.SUCCESS) {
+                confirmAppConnected();
+            }
+            else {
+                confirmCantConnect();
+            }
+        }
+        if (requestCode == FitnessApps.FITBIT_INIT_REQUEST_CODE) {
+            if (FitbitStringsSavedData.getInstance() != null &&
+                    FitbitStringsSavedData.getInstance().getTopData() != null) {
+                confirmAppConnected();
+            }
+            else {
+                confirmCantConnect();
+            }
+        }
     }
 
     /**
@@ -540,138 +754,6 @@ public class FirstMenu extends AppCompatActivity implements IntegrationConnectio
                         .setDuration(Numbers.FIRST_MENU_FITNESS_ANIM_DURATION)
         );
         hideLayoutSet.start();
-    }
-
-    void populateFitnessAppList() {
-        final ArrayList<FitnessAppSmallCard> fitnessApps = new ArrayList<>();
-        boolean offsetColour = false;
-
-        for (int i = 0; i < FitnessApps.NUM_OF_APPS; i++) {
-            FitnessAppSmallCard item = new FitnessAppSmallCard();
-            item.setAppName(FitnessApps.getFitnessAppName(i));
-            item.setAppImage(FitnessApps.getFitnessAppImage(i));
-            if (offsetColour) {
-                item.setBackgroundColour(R.color.colorPrimary);
-                offsetColour = false;
-            }
-            else {
-                item.setBackgroundColour(R.color.colorPrimary);
-                offsetColour = true;
-            }
-
-            fitnessApps.add(item);
-
-        }
-
-        final FitnessAppSmallCardAdapter adapter = new FitnessAppSmallCardAdapter(getApplicationContext(), fitnessApps);
-        fitnessAppsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                FitnessAppSmallCard item = (FitnessAppSmallCard) adapterView.getItemAtPosition(i);
-                selectFitnessApp(FitnessApps.getFitnessAppFromName(item.getAppName()));
-            }
-        });
-        fitnessAppsList.setAdapter(adapter);
-
-        ViewGroup.LayoutParams layoutParams = fitnessAppsLayout.getLayoutParams();
-        layoutParams.width = (int) (jamesImage.getX() - Numbers.FIRST_MENU_FITNESS_MARGIN_PADDING);
-        fitnessAppsLayout.setLayoutParams(layoutParams);
-        fitnessAppsLayout.requestLayout();
-
-    }
-
-    void populateSpinner() {
-        List<String> spinnerList = new ArrayList<>();
-
-        if (gameMode == GameModes.TEAM_MODE) {
-            for (int i = 1; i <= Colours.NUM_TEAM_COLOURS; i++) {
-                spinnerList.add(Colours.getColourName(i));
-            }
-        }
-        else {
-            spinnerList = Words.teamNames;
-            Collections.sort(spinnerList);
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, R.layout.spinner_item, spinnerList);
-        teamColourSpinner.setAdapter(adapter);
-    }
-
-    void populateStepTargetSpinner() {
-        List<String> spinnerList = new ArrayList<>();
-        for (int i = 1; i <= Numbers.MAX_NUM_STEPS_TARGET; i++) {
-            spinnerList.add(String.valueOf(i));
-        }
-        ArrayAdapter<String> stepAdapter = new ArrayAdapter<>(
-                this, R.layout.spinner_item, spinnerList);
-        stepTargetSpinner.setAdapter(stepAdapter);
-    }
-
-    void populateDaysBetweenSpinner() {
-        List<String> spinnerList = new ArrayList<>();
-        for (int i = 2; i <= Numbers.MAX_NUM_DAYS_BETWEEN; i+=2) {
-            spinnerList.add(String.valueOf(i));
-        }
-        ArrayAdapter<String> daysAdapter = new ArrayAdapter<>(
-                this, R.layout.spinner_item, spinnerList);
-        daysBetweenSpinner.setAdapter(daysAdapter);
-    }
-
-
-    void selectFitnessApp(int app) {
-        switch (app) {
-            case FitnessApps.MOCKED:
-                hideMenu(fitnessAppsLayout);
-                putSpeechBubbleBackIntoMiddle();
-                confirmAppConnected();
-                break;
-            case FitnessApps.GOOGLE_FIT:
-                speechBubbleText.setText(R.string.first_menu_calling_app);
-                hideMenu(fitnessAppsLayout);
-                putSpeechBubbleBackIntoMiddle();
-                GoogleFitAPI.createGoogleFitAPIInstance(this);
-                if (GoogleFitAPI.getInstance().hasPermissions(this)) {
-                    confirmAppConnected();
-                }
-                break;
-            case FitnessApps.FITBIT:
-                speechBubbleText.setText(R.string.first_menu_calling_app);
-                hideMenu(fitnessAppsLayout);
-                putSpeechBubbleBackIntoMiddle();
-                FitbitStringsSavedData.createFitbitStringsSavedDataInstance(this);
-                FitbitAPI.createFitbitAPIInstance(this);
-                FitbitAPI.getInstance().requestFitbitPermission(this);
-                break;
-            default:
-                speechBubbleText.setText(R.string.first_menu_app_connected);
-        }
-        Settings.getInstance().changeFitnessApp(app);
-
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == FitnessApps.GOOGLE_FIT_INIT_REQUEST_CODE) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            int statusCode = result.getStatus().getStatusCode();
-            if (statusCode == GoogleSignInStatusCodes.SUCCESS) {
-                confirmAppConnected();
-            }
-            else {
-                confirmCantConnect();
-            }
-        }
-        if (requestCode == FitnessApps.FITBIT_INIT_REQUEST_CODE) {
-            if (FitbitStringsSavedData.getInstance() != null &&
-                    FitbitStringsSavedData.getInstance().getTopData() != null) {
-                confirmAppConnected();
-            }
-            else {
-                confirmCantConnect();
-            }
-        }
     }
 
 
